51338e7c6b86b305fa797df35e40eb5108c1bf56
Nagaraju Devarasetty <dmvnraju38@gmail.com>
2022-01-26 19:30:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {Greet} from './Components/Greet';
+import Welcome from './Components/Welcome';
+
 function App() {
+  const actualName = 'Nagaraju';
   return (
     <div className="App">
+      <Greet name={actualName} test=""></Greet>
+      <Welcome />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Components/Greet.js
@@ -0,0 +1,10 @@
+
+// Function
+// function Greet() {
+//     return <h1>Greet! Hello Nagaraju</h1>
+// }
+
+// ES6 arrow function
+export const Greet = ({name, test}) =>  <h1>Greet! Hello {name} {test}</h1>
+
+// export default Greet;
